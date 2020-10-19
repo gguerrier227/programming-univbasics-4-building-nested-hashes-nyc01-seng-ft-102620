4a31a95b1ca019a256fc151c675f40142475609f
Gueshneider Guerrier <gguerrier227@gmail.com>
2020-10-19 14:25:40 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -6,10 +6,10 @@ monopoly = {
     :pieces => 4
     :rent_in_dollars =>
     {
-      :one_piece_owned => 25
-      :two_pieces_owned => 50
-      :three_peices_owned => 100
-      :four_pieces_owned => 200
+      :one_piece_owned => 25,
+      :two_pieces_owned => 50,
+      :three_peices_owned => 100,
+      :four_pieces_owned => 200,
     }
   }
 }
