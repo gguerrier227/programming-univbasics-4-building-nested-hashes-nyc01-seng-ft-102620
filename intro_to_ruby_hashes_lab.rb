d48449819ca5922c1e3c3a249e83508c279d771c
Gueshneider Guerrier <gguerrier227@gmail.com>
2020-10-19 14:33:10 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -3,6 +3,16 @@ def base_hash
   # keyword "return." Nice and neat.
 monopoly = {
   :railroads => {
+}
+end
+
+def monopoly_with_second_tier
+  # When you start writing the implementation for this method, copy the Hash
+  # you built in the previous method. Run the tests. They will fail, but
+  # they'll guide you in how to modify what you just did to have more
+  # complexity.
+monopoly = {
+  :railroads => {
     :pieces => 4
     :rent_in_dollars =>
     {
@@ -14,16 +24,7 @@ monopoly = {
     :names => 
     }
   }
-}
-end
-
-def monopoly_with_second_tier
-  # When you start writing the implementation for this method, copy the Hash
-  # you built in the previous method. Run the tests. They will fail, but
-  # they'll guide you in how to modify what you just did to have more
-  # complexity.
-monopoly = {
-  :railroads => {}
+  }
 }
 
 end
